0247364feb058d73a8e320019cd7a98d5625f755
Sefinsha-b <Sefinsha4@gmail.com>
2023-09-15 03:06:11 +0530

    Added new feature: Create .envn

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
-const cors = require('cors'); // Import cors middleware
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000; // Set the port
+
 const mongoose = require('mongoose');
 const Vetroutes = require('./Routes/Vetapi');
 const Petapiroutes = require('./Routes/Petapi');
 const Appointmentroutes = require('./Routes/appointmentapi');
 const Departmentroutes = require('./Routes/Departmentapi');
-const Dummyroutes=require('./Routes/dummapi.js')
+const Dummyroutes = require('./Routes/dummapi.js');
 
-// Allow requests from http://localhost:4200
-const allowedOrigins = ['http://localhost:4200','http://localhost:51714'];
+const allowedOrigins = ['http://localhost:4200'];
 
-// Configure CORS with specific origin(s) and methods
 app.use(
   cors({
     origin: function (origin, callback) {
       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        // If the origin is in the allowed list or is undefined (e.g., a same-origin request), allow it
         callback(null, true);
       } else {
-        // If the origin is not in the allowed list, reject the request
         callback(new Error('Not allowed by CORS'));
       }
     },
-    methods: ['*'], // Allow all HTTP methods
+    methods: ['*'],
   })
 );
 
-
-
-// Middleware
 app.use(express.json());
 
-// Define a basic route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
@@ -44,26 +39,20 @@ app.use('/Appointments', Appointmentroutes);
 app.use('/DepartMents', Departmentroutes);
 app.use('/Dummy', Dummyroutes);
 
-
-// MongoDB Connection Parameters
-const dbUrl =
-  'mongodb+srv://Sefin_clinis:N7zys0g7htbYFZ6W@cluster1.sulequc.mongodb.net/?retryWrites=true&w=majority';
+const dbUrl = process.env.DATABASE_URL; // Use the environment variable
 
 const connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-// MongoDB Connection
 mongoose
   .connect(dbUrl, connectionParams)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
-    // Optionally, exit the application or take other error-handling actions
   });
 
-// Error Handling for CORS
 app.use((err, req, res, next) => {
   if (err.name === 'CorsError') {
     res.status(403).json({ message: 'CORS error: Not allowed by CORS' });
@@ -72,13 +61,11 @@ app.use((err, req, res, next) => {
   }
 });
 
-// Global Error Handling Middleware
 app.use((err, req, res, next) => {
   console.error('Error:', err);
   res.status(500).json({ message: 'Internal Server Error' });
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
